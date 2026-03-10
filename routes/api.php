d346a444bc58d5d1982798712ba8260eb28e8526
Rilo Arbabillah <rilo.arbabillah@gmail.com>
2026-03-11 03:03:55 +0800

    feat: add REST API endpoints for CrowdSec (#14) (#32)
    - Add CrowdSecApiController (stats, events, blocked, block, unblock, check)
    - Add routes/api.php with configurable middleware
    - Add isWhitelisted() method to CrowdSecService
    - API disabled by default (enable via CROWDSEC_API_ENABLED=true)

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use RiloArbabillah\LaravelCrowdSec\Http\Controllers\CrowdSecApiController;
+
+Route::prefix('api/crowdsec')
+    ->middleware(config('crowdsec-scenarios.api.middleware', ['api']))
+    ->group(function () {
+        Route::get('/stats', [CrowdSecApiController::class, 'stats']);
+        Route::get('/events', [CrowdSecApiController::class, 'events']);
+        Route::get('/blocked', [CrowdSecApiController::class, 'blocked']);
+        Route::post('/block', [CrowdSecApiController::class, 'block']);
+        Route::delete('/block/{ip}', [CrowdSecApiController::class, 'unblock']);
+        Route::get('/check/{ip}', [CrowdSecApiController::class, 'check']);
+    });
